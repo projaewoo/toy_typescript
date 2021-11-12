b0b21f5e0334f505f67043cd47de61fc72d00b28
jeongjw <jw.jeong@sqisoft.com>
2021-11-12 09:21:49 +0900

    redux 적용

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+// 스토어 생성
+import { createStore } from "redux";
+// Provider 컴포넌트를 이용해서 리덕스 적용
+import { Provider } from "react-redux";
+// 패키지 추가해서 리덕스 적용
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import rootReducer from "./modules";
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
