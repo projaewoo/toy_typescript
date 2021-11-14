d4172708593e8d6728a56ba1f714d3a58fffbfa1
정재우 <>
2021-11-14 19:47:56 +0900

    리덕스에 typescript 적용중

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -2,6 +2,8 @@ import { combineReducers } from "redux";
 import counter from "./counter";
 import todos from "./todos";
 
+export type RootState = ReturnType<typeof rootReducer>
+
 const rootReducer = combineReducers({
   counter,
   todos,
